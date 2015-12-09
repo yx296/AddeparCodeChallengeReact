1860449e900b15cfb0f4133d1618ecb8b589cd5b
Yilin Xu <yx296@nyu.edu>
2015-12-09 01:56:35 -0800

    install dev server

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+var path = require('path');
+var HtmlwebpackPlugin = require('html-webpack-plugin');
+
+const PATHS = {
+  app: path.join(__dirname, 'app'),
+  build: path.join(__dirname, 'build')
+}
+
+module.exports = {
+  entry: PATHS.app,
+  output: {
+    path: PATHS.build,
+    filename: 'bundle.js'
+  },
+  plugins: [
+    new HtmlwebpackPlugin({
+      title: 'Kanban app'
+    })
+  ]
+};
