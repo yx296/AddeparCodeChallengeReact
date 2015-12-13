bfd9468e55b3c02808a839fe6c61f0798e9393cb
Yilin Xu <yx296@nyu.edu>
2015-12-13 13:24:21 -0800

    more kanban

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -3,30 +3,70 @@ import React from 'react';
 
 import Notes from './Notes.jsx';
 
-const notes = [
-  {
-    id: uuid.v4(),
-    task: 'Learn Webpack'
-  },
-  {
-    id: uuid.v4(),
-    task: 'Learn React'
-  },
-  {
-    id: uuid.v4(),
-    task: 'Do laundry'
-  }
-];
+
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      notes: [
+        {
+          id: uuid.v4(),
+          task: 'Learn Webpack'
+        },
+        {
+          id: uuid.v4(),
+          task: 'Learn React'
+        },
+        {
+          id: uuid.v4(),
+          task: 'Do laundry'
+        }
+      ]
+    }
+  }
   render() {
+    const notes = this.state.notes;
     return (
       <div>
+        <button className="add-note" onClick={this.addNote}> + </button>
         <Notes items={notes}/>
       </div>
     )
   }
+
+  addNote = () => {
+    this.setState({
+      notes: this.state.notes.concat([{
+        id: uuid.v4(),
+        task: 'New task'
+      }])
+    });
+  }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
